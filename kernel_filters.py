00239fbc59e7740187da812afd8c88b4e55e3834
Mario Pérez <48880789+mapecode@users.noreply.github.com>
2020-05-24 11:50:17 +0200

    version con 3 bucles

--- kernel_filters.py
+++ kernel_filters.py
@@ -15,9 +15,8 @@ def convolve2d(image, kernel, mult):
 
     for x in range(dim, image_h - dim):
         for y in range(dim, image_w - dim):
-            for m in range(dim):
-                for n in range(dim):
-                    output[x][y] += kernel[m][n] * image[x - dim + m][y - dim + n]
+            for i in range(dim):
+                output[x][y] += kernel[i // dim][i % dim] * image[x - dim + (i // dim)][y - dim + (i % dim)]
 
             output[x][y] //= mult
 
@@ -27,6 +26,6 @@ def convolve2d(image, kernel, mult):
 if __name__ == '__main__':
     start = time.time()
     img = cv2.cvtColor(cv2.imread(sys.argv[1]), cv2.COLOR_BGR2GRAY)
-    new_img = convolve2d(img, kernel=k["bottom_sobel"][0], mult=k["bottom_sobel"][1])
+    new_img = convolve2d(img, kernel=k["gaussian_blur5x5"][0], mult=k["gaussian_blur5x5"][1])
     cv2.imwrite('./result.jpg', new_img)
     print(round(time.time() - start, 2))
