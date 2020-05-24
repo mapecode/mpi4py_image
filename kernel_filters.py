1e38d62fe8ba1003bd35d4197d2b8a419f86910e
Mario Pérez <48880789+mapecode@users.noreply.github.com>
2020-05-24 12:34:22 +0200

    version recursiva

--- kernel_filters.py
+++ kernel_filters.py
@@ -6,6 +6,15 @@ from kernels import KERNELS as k
 
 
 def convolve2d(image, kernel, mult):
+    def mult_matrix(kernel, image, x, y, i, dim):
+        if i < dim * dim:
+            m = i // dim
+            n = i % dim
+            result = kernel[m][n] * image[x - dim + m][y - dim + n]
+            return result + mult_matrix(kernel, image, x, y, i + 1, dim)
+        else:
+            return 0
+
     dim = kernel.shape[0]
     image_h = image.shape[0]
     image_w = image.shape[1]
@@ -15,11 +24,7 @@ def convolve2d(image, kernel, mult):
 
     for x in range(dim, image_h - dim):
         for y in range(dim, image_w - dim):
-            for i in range(dim*dim):
-                m = i // dim
-                n = i % dim
-                output[x][y] += kernel[m][n] * image[x - dim + m][y - dim + n]
-
+            output[x][y] = mult_matrix(kernel, image, x, y, 0, dim)
             output[x][y] //= mult
 
     return output
@@ -28,6 +33,6 @@ def convolve2d(image, kernel, mult):
 if __name__ == '__main__':
     start = time.time()
     img = cv2.cvtColor(cv2.imread(sys.argv[1]), cv2.COLOR_BGR2GRAY)
-    new_img = convolve2d(img, kernel=k["sharpen"][0], mult=k["sharpen"][1])
+    new_img = convolve2d(img, kernel=k["emboss"][0], mult=k["emboss"][1])
     cv2.imwrite('./result.jpg', new_img)
     print(round(time.time() - start, 2))
